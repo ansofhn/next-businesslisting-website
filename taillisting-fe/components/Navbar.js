94d876ebc6f6b9866de57d96535970f6dedfea52
Ansof Habibunnadjar <ansofhn5@gmail.com>
2023-05-22 13:00:17 +0700

    feat: add navbar n hero banner

--- taillisting-fe/components/Navbar.js
+++ taillisting-fe/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from "react";
+import hero from "../public/assets/images/hero/banner_3.jpg";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div>
-      <nav>
+      <nav className="absolute left-0 right-0 z-10">
         <div className="flex items-center justify-between w-9/12 py-5 mx-auto">
           <div>
             <a href="#" className="text-lg">
@@ -21,6 +23,47 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
+      <div className="relative h-screen">
+        <Image src={hero} className="object-cover w-full h-full" />
+        {/* <img src={hero} className="object-cover w-full h-full" /> */}
+        <div className="absolute inset-0 bg-black/50"></div>
+        <div className="absolute inset-0 w-full h-full">
+          <div className="flex items-center justify-center w-full min-h-full">
+            <div>
+              <div className="mb-4 text-center text-white">
+                <p className="mb-4 text-4xl font-bold capitalize">
+                  Discover places that people love
+                </p>
+                <p className="text-lg">
+                  We will help you to find the best places in the World.
+                </p>
+              </div>
+              <div className="flex gap-5 ">
+                <div>
+                  <input
+                    type="text"
+                    placeholder="location"
+                    className="px-4 py-3 text-gray-600 bg-white rounded-md w-80 focus:outline-none"
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    placeholder="I'm Looking for..."
+                    className="px-4 py-3 text-gray-600 bg-white rounded-md w-80 focus:outline-none"
+                  />
+                </div>
+                <div>
+                  <button className="rounded-md py-2.5 px-6 bg-amber-500 text-lg text-white hover:bg-amber-600">
+                    Search
+                  </button>
+                </div>
+              </div>
+              <div></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
